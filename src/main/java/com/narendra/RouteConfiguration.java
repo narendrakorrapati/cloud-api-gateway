e9037f197a06a95bcd3045a777372c8b4f09b53b
narendrakorrapati <korrapatinarendra843@gmail.com>
2024-01-25 12:19:24 +0530

    Eureka update

--- src/main/java/com/narendra/RouteConfiguration.java
+++ src/main/java/com/narendra/RouteConfiguration.java
@@ -1,5 +1,6 @@
 package com.narendra;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -7,13 +8,16 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RouteConfiguration {
+	
+	@Value("${microservices.endpoint.config-client}")
+	private String configClientURL;
 
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(prespec -> prespec.path("/config-client/**")
 						.filters(gatewayFilter -> gatewayFilter.addRequestHeader("Hello", "World"))
-						.uri("http://localhost:8082"))
+						.uri(configClientURL))
 				.build();
 	}
 }
