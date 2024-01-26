cbcacca444adc0b964efbd70df6bb9d784f47164
narendrakorrapati <korrapatinarendra843@gmail.com>
2024-01-26 09:47:10 +0530

    Routing for two new services

--- src/main/java/com/narendra/RouteConfiguration.java
+++ src/main/java/com/narendra/RouteConfiguration.java
@@ -11,13 +11,24 @@ public class RouteConfiguration {
 	
 	@Value("${microservices.endpoint.config-client}")
 	private String configClientURL;
+	
+	@Value("${microservices.endpoint.settlement-service}")
+	private String settlementServiceURL;
+	
+	@Value("${microservices.endpoint.dispute-service}")
+	private String dispuerServiceURL;
+	
 
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
-				.route(prespec -> prespec.path("/config-client/**")
+				.route("config-client", prespec -> prespec.path("/config-client/**")
 						.filters(gatewayFilter -> gatewayFilter.addRequestHeader("Hello", "World"))
 						.uri(configClientURL))
+				.route("settlement-service", prespec -> prespec.path("/settlement-service/**")
+						.uri(settlementServiceURL))
+				.route("disput-service", prespec -> prespec.path("/dispute-service/**")
+						.uri(dispuerServiceURL))
 				.build();
 	}
 }
